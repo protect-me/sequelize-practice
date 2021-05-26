900391ad4c2d85eb24ef838557cb893e51111789
protect-me <pmfwiww@gmail.com>
2021-05-26 15:34:45 +0900

    Feat: connect DB, create Model

--- app.js
+++ app.js
@@ -1,91 +1,98 @@
-const express = require('express');
-const nunjucks = require('nunjucks');
-const logger = require('morgan');
-const bodyParser = require('body-parser');
+const express = require("express");
+const nunjucks = require("nunjucks");
+const logger = require("morgan");
+const bodyParser = require("body-parser");
 
+// db 관련
+const db = require("./models");
 
 class App {
+  constructor() {
+    this.app = express();
 
-    constructor () {
-        this.app = express();
-        
-        // 뷰엔진 셋팅
-        this.setViewEngine();
+    // db 접속
+    this.dbConnection();
 
-        // 미들웨어 셋팅
-        this.setMiddleWare();
+    // 뷰엔진 셋팅
+    this.setViewEngine();
 
-        // 정적 디렉토리 추가
-        this.setStatic();
+    // 미들웨어 셋팅
+    this.setMiddleWare();
 
-        // 로컬 변수
-        this.setLocals();
+    // 정적 디렉토리 추가
+    this.setStatic();
 
-        // 라우팅
-        this.getRouting();
+    // 로컬 변수
+    this.setLocals();
 
-        // 404 페이지를 찾을수가 없음
-        this.status404();
+    // 라우팅
+    this.getRouting();
 
-        // 에러처리
-        this.errorHandler();
+    // 404 페이지를 찾을수가 없음
+    this.status404();
 
+    // 에러처리
+    this.errorHandler();
+  }
 
-    }
+  dbConnection() {
+    // DB authentication
+    db.sequelize
+      .authenticate()
+      .then(() => {
+        console.log("Connection has been established successfully.");
+      })
+      .then(() => {
+        console.log("DB Sync complete.");
+        return db.sequelize.sync();
+      })
+      .catch((err) => {
+        console.error("Unable to connect to the database:", err);
+      });
+  }
 
+  setMiddleWare() {
+    // 미들웨어 셋팅
+    this.app.use(logger("dev"));
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+  }
 
-    setMiddleWare (){
-        
-        // 미들웨어 셋팅
-        this.app.use(logger('dev'));
-        this.app.use(bodyParser.json());
-        this.app.use(bodyParser.urlencoded({ extended: false }));
+  setViewEngine() {
+    nunjucks.configure("template", {
+      autoescape: true,
+      express: this.app,
+    });
+  }
 
-    }
+  setStatic() {
+    this.app.use("/uploads", express.static("uploads"));
+  }
 
-    setViewEngine (){
+  setLocals() {
+    // 템플릿 변수
+    this.app.use((req, res, next) => {
+      this.app.locals.isLogin = true;
+      this.app.locals.req_path = req.path;
+      next();
+    });
+  }
 
-        nunjucks.configure('template', {
-            autoescape: true,
-            express: this.app
-        });
+  getRouting() {
+    this.app.use(require("./controllers"));
+  }
 
-    }
+  status404() {
+    this.app.use((req, res, _) => {
+      res.status(404).render("common/404.html");
+    });
+  }
 
-
-    setStatic (){
-        this.app.use('/uploads', express.static('uploads'));
-    }
-
-    setLocals(){
-
-        // 템플릿 변수
-        this.app.use( (req, res, next) => {
-            this.app.locals.isLogin = true;
-            this.app.locals.req_path = req.path;
-            next();
-        });
-
-    }
-
-    getRouting (){
-        this.app.use(require('./controllers'))
-    }
-
-    status404() {        
-        this.app.use( ( req , res, _ ) => {
-            res.status(404).render('common/404.html')
-        });
-    }
-
-    errorHandler() {
-
-        this.app.use( (err, req, res,  _ ) => {
-            res.status(500).render('common/500.html')
-        });
-    
-    }
-
+  errorHandler() {
+    this.app.use((err, req, res, _) => {
+      res.status(500).render("common/500.html");
+    });
+  }
 }
 
 module.exports = new App().app;
