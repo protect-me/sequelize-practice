cda0ccd3298667f5152c21fcd7e30d1e4b93c55a
protect-me <pmfwiww@gmail.com>
2021-05-27 08:56:29 +0900

    Feat: set Model

--- controllers/admin/admin.ctrl.js
+++ controllers/admin/admin.ctrl.js
@@ -1,13 +1,27 @@
-exports.get_products = ( _ , res) => {
-    res.render( 'admin/products.html' , 
-        { message : "hello" } // message 란 변수를 템플릿으로 내보낸다.
-    );
-}
+const models = require("../../models");
 
-exports.get_products_write = ( _ , res) => {
-    res.render( 'admin/write.html');
-}
+exports.get_products = (_, res) => {
+  res.render(
+    "admin/products.html",
+    { message: "hello" } // message 란 변수를 템플릿으로 내보낸다.
+  );
+};
 
-exports.post_products_write = ( req , res ) => {
-    res.send(req.body);
-}
+exports.get_products_write = (_, res) => {
+  res.render("admin/write.html");
+};
+
+exports.post_products_write = (req, res) => {
+  //   res.send(req.body);
+
+  //   models.Products.create({
+  //     name: req.body.name,
+  //     price: req.body.price,
+  //     description: req.body.description,
+  //   }).then(() => {
+  //     res.redirect("/admin/products");
+  //   });
+  models.Products.create(req.body).then(() => {
+    res.redirect("/admin/products");
+  });
+};
